60253858b4ecdc6107ab6f8c2b288ee36aaa8c7f
Chamith Siriwardena <chamith@gmail.com>
2022-03-23 07:26:18 +0530

    removing unused imports

--- src/test/java/org/testunited/examples/learnright/booktitle/test/contract/BookTitleBase.java
+++ src/test/java/org/testunited/examples/learnright/booktitle/test/contract/BookTitleBase.java
@@ -1,11 +1,9 @@
 package org.testunited.examples.learnright.booktitle.test.contract;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -15,7 +13,6 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import org.testunited.examples.learnright.booktitle.BookTitle;
